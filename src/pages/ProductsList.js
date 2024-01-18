cafe9c2bc971d14663e9458aa064bb3045b719d8
Abd Elrhman Mohamed Shata <abdoelrhmanshata@yahoo.com>
2024-01-18 23:42:47 +0200

    Update Product Page

--- src/pages/ProductsList.js
+++ src/pages/ProductsList.js
@@ -1,18 +1,14 @@
-import { useState } from "react";
 import Search from "../components/Search";
 import Categories from "../components/Categories";
 import PaginationPage from "../components/PaginationPage";
 import Products from "../components/Products";
 export default function ProductsList() {
-  const [category, setCategory] = useState("all");
   return (
     <div className="container-fluid px-5 py-4">
       <Search />
-      <Categories
-        categorySelected={(categoryName) => setCategory(categoryName)}
-      />
+      <Categories />
       <Products />
-      <PaginationPage categorySelected={category} />
+      <PaginationPage />
     </div>
   );
 }
