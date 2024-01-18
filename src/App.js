16085c457d8f48422174b2025cbd11cfe5e0da65
Abd Elrhman Mohamed Shata <abdoelrhmanshata@yahoo.com>
2024-01-18 23:42:06 +0200

    add Products Context in app

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import { BrowserRouter } from "react-router-dom";
 import Header from "./components/Header";
 import Router from "./Router/Router";
+import ProductsContext from "./context/products";
+import { useState } from "react";
 
 function App() {
+  const [productsDetails, setProductsDetails] = useState({
+    category: "all",
+    page: 1,
+  });
   return (
-    <BrowserRouter>
-      <Header />
-      <Router />
-    </BrowserRouter>
+    <ProductsContext.Provider value={{ productsDetails, setProductsDetails }}>
+      <BrowserRouter>
+        <Header />
+        <Router />
+      </BrowserRouter>
+    </ProductsContext.Provider>
   );
 }
 
