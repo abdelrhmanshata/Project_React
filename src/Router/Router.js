9a19c52430dc3aea304a21fb3245d09222616e1e
Abd Elrhman Mohamed Shata <abdoelrhmanshata@yahoo.com>
2024-01-19 15:50:57 +0200

    Update Router.js

--- src/Router/Router.js
+++ src/Router/Router.js
@@ -3,18 +3,18 @@ import { Route, Routes } from "react-router-dom";
 
 const ProductsList = React.lazy(() => import("../pages/ProductsList"));
 const SearchProducts = React.lazy(() => import("../pages/SearchProducts"));
-const NotFound = React.lazy(() => import("../pages/NotFound"));
 const ShowProduct = React.lazy(() => import("../pages/ShowProduct"));
-
+const Cart = React.lazy(() => import("../pages/Cart"));
+const NotFound = React.lazy(() => import("../pages/NotFound"));
 export default function Router() {
   return (
     <Suspense fallback={<h5>Loading ...</h5>}>
       <Routes>
         <Route path="" element={<ProductsList />} />
         <Route path="/search-products/:name" element={<SearchProducts />} />
-        <Route path="*" element={<NotFound />} />
+        <Route path="/cart-products/" element={<Cart />} />
         <Route path="/show-products/:id" element={<ShowProduct />} />
-
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </Suspense>
   );
