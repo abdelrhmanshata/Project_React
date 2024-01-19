b3d4aaae3ccc788f60e8a68785574e363d49e47d
mohamed7855 <martm7726@gmail.com>
2024-01-19 13:05:35 +0200

    Filter Categories

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,17 +1,25 @@
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import Product from "./Product";
 import { axiosInstance } from "../api/config";
+import ProductsContext from "../context/products";
 export default function Products() {
   const [products, setProducts] = useState([]);
-
+  const { productsDetails } = useContext(ProductsContext);
   useEffect(() => {
-    axiosInstance
-      .get("")
-      .then((res) => {
-        setProducts(res.data.products);
-      })
-      .catch((err) => console.log(err));
-  }, []);
+    productsDetails.category === "all"
+      ? axiosInstance
+          .get(`?limit=12&skip=${(productsDetails.page - 1) * 12}`)
+          .then((res) => {
+            setProducts(res.data.products);
+          })
+          .catch((err) => console.log(err))
+      : axiosInstance
+          .get(`category/${productsDetails.category}`)
+          .then((res) => {
+            setProducts(res.data.products);
+          })
+          .catch((err) => console.log(err));
+  }, [productsDetails]);
 
   return (
     <>
