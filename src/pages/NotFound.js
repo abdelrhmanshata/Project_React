6bc4cbb033574b4c1c5d0aff2bf6a9e6739829b5
Abd Elrhman Mohamed Shata <abdoelrhmanshata@yahoo.com>
2024-01-19 16:32:00 +0200

    Update Not Found

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,7 +1,7 @@
 import notFound from "../images/notFound.png";
 export default function NotFound() {
   return (
-    <div className="d-flex flex-column align-items-center justify-content-center gap-3">
+    <div className="d-flex flex-column align-items-center justify-content-center gap-3 mt-5">
       <h1 className="mt-5 text-primary">Not Found Page</h1>
       <img src={notFound} alt="404 Not Found" width="500px" height="500px" />
     </div>
