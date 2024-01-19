1c792bf62f2e6191a4b4730938dbbf3daf0f5a59
Abd Elrhman Mohamed Shata <abdoelrhmanshata@yahoo.com>
2024-01-19 15:37:37 +0200

    Updaet NotFound Page

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,12 +1,9 @@
+import notFound from "../images/notFound.png";
 export default function NotFound() {
   return (
-    <div>
-      <img
-        src="https://media.istockphoto.com/id/1207750534/photo/404-error-internet-page-not-found-hand-with-magnifier-concept-picture.jpg?s=612x612&w=0&k=20&c=-eDOz1tb4CC8dqgcggiyA6A6ozlR_DEtaeyQI8zU_FU="
-        alt="404 Not Found"
-        width="100%"
-        height="100%"
-      />
+    <div className="d-flex flex-column align-items-center justify-content-center gap-3">
+      <h1 className="mt-5 text-primary">Not Found Page</h1>
+      <img src={notFound} alt="404 Not Found" width="500px" height="500px" />
     </div>
   );
 }
